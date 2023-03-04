8791b0702db9635ba1e08c97969da98825908650
EC2 Default User <ec2-user@ip-172-31-30-185.ap-northeast-1.compute.internal>
2023-03-04 15:53:57 +0000

    following_fix

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -14,11 +14,12 @@ class RelationshipsController < ApplicationController
   
   def followings
     @user = User.find(params[:user_id])
-    @relationships = Relationship.all
+    @users = @user.followings
   end
   # フォロワー一覧
   def followers
     @user = User.find(params[:user_id])
+    @users = @user.followers
   end
   
 end
