9043c725e5d9d1153bb55baa12d09cdfc2455116
EC2 Default User <ec2-user@ip-172-31-30-185.ap-northeast-1.compute.internal>
2023-03-06 07:42:09 +0000

    favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,17 @@
 class FavoritesController < ApplicationController
     def create
-     book = Book.find(params[:book_id])
-     favorite = current_user.favorites.new(book_id: book.id)
+     @book = Book.find(params[:book_id])
+     favorite = current_user.favorites.new(book_id: @book.id)
      favorite.save
-     redirect_to request.referer
-     #遷移元のURLを取得してリダイレクトする
+     render 'favorite.js.erb' 
     end
     
     def destroy
-      book = Book.find(params[:book_id])
-      favorite = current_user.favorites.find_by(book_id: book.id)
+      @book = Book.find(params[:book_id])
+      favorite = current_user.favorites.find_by(book_id: @book.id)
       #現在ログインしているユーザのFavoritの中でbook_idとbook.idが一致するもの取得
       favorite.destroy
-      redirect_to request.referer
+      render 'favorite.js.erb' 
     end
     
     def follows
