26ae37c5e45df6a8b919a06ace9034a6f0fa1832
EC2 Default User <ec2-user@ip-172-31-30-185.ap-northeast-1.compute.internal>
2023-03-11 06:35:53 +0000

    fix

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,16 @@
 class Group < ApplicationRecord
-    has_many :group_users
-    has_many :users, through: :group_users
-    validates :name, presence: true
-    validates :introduction, presence: true
-    attachment :image, destroy: false
-    
+  has_one_attached :image
+  belongs_to :owner, class_name: 'User'
+  has_many :group_users, dependent: :destroy
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+
+  def get_image
+    (image.attached?) ? image : 'no_image.jpg'
+  end
+  
+  def is_owned_by?(user)
+    owner.id == user.id
+  end
 end
